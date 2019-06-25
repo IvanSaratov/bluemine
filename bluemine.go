c4e580f88f8571cf966c3af76de987515fca45bc
ivan <dfeqcn11@gmail.com>
2019-06-25 12:28:28 +0400

    Move from http to mux functionality and added servestatic method

--- bluemine.go
+++ bluemine.go
@@ -4,11 +4,16 @@ import (
 	"database/sql"
 	"flag"
 	"log"
+	"os"
 	"net/http"
+	"io"
+	"strings"
 
 	"github.com/IvanSaratov/bluemine/config"
 	"github.com/IvanSaratov/bluemine/handler"
 	//"github.com/IvanSaratov/bluemine/session"
+
+	"github.com/gorilla/mux"
 	_ "github.com/cockroachdb/cockroach-go/crdb"
 )
 
@@ -29,10 +34,12 @@ func main() {
 	}
 	defer db.Close()
 
-	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+	router := mux.NewRouter()
 
-	http.HandleFunc("/login", handler.LoginHandler)
-	http.HandleFunc("/logout", handler.LoginHandler)
+	router.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+
+	router.HandleFunc("/login", handler.LoginHandler)
+	router.HandleFunc("/logout", handler.LoginHandler)
 
 	go listen(config.Conf.Bind)
 
@@ -42,4 +49,23 @@ func main() {
 
 func listen(addr string) {
 	log.Fatal("ListenAndServe: ", http.ListenAndServe(addr, nil))
+}
+
+func serveStatic(filename string, w http.ResponseWriter) {
+	file, err := os.Open("filename")
+	if err != nil {
+		w.WriteHeader(404)
+		w.Write([]byte("Could not find file: " + filename))
+		return
+	}
+	defer file.Close()
+
+	if strings.HasSuffix(filename, ".css") {
+		w.Header().Add("Content-type", "text/css")
+	} else if strings.HasSuffix(filename, ".js") {
+		w.Header().Add("Content-type", "application/javascript")
+	}
+
+	io.Copy(w, file)
+
 }
