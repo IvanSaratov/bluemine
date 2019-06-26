a5b652dfee9f610a29e2c19d2b82d8dd94ef5cff
ivan <dfeqcn11@gmail.com>
2019-06-26 22:52:46 +0400

    Deleted old loginHandler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,12 +1,7 @@
 package handlers
 
 import (
-	"crypto/md5"
-	"crypto/sha1"
-	"errors"
-	"fmt"
 	"html/template"
-	"io"
 	"net/http"
 
 	"github.com/IvanSaratov/bluemine/data"
@@ -23,25 +18,4 @@ func UserProfileHandler(w http.ResponseWriter, r *http.Request) {
 
 	tmpl, _ := template.ParseFiles("public/html/profile.html")
 	tmpl.Execute(w, data)
-}
-
-func loginUser(userLogin, userPassword string) (sessionId string, err error) {
-	var password string
-
-	if passwordHash(userPassword) != password {
-		err = errors.New("Incorrect password")
-		return
-	}
-
-	return
-}
-
-func passwordHash(password string) string {
-	sh := sha1.New()
-	io.WriteString(sh, password)
-
-	md := md5.New()
-	io.WriteString(md, password)
-
-	return fmt.Sprintf("%x:%x", sh.Sum(nil), md.Sum(nil))
 }
