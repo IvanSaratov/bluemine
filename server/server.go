344f4c382039eb3fb8866036ec80c536b6b82a77
ivan <dfeqcn11@gmail.com>
2019-06-26 14:55:04 +0400

    Deleted old session functional

--- server/server.go
+++ server/server.go
@@ -2,29 +2,17 @@ package server
 
 import (
 	"database/sql"
-	"encoding/json"
 	"errors"
-	"fmt"
-	"math/rand"
-	"time"
 
 	"github.com/gorilla/sessions"
 
 	"github.com/IvanSaratov/bluemine/config"
-	"github.com/bradfitz/gomemcache/memcache"
 )
 
 var Core struct {
 	DB    *sql.DB
 	Store *sessions.CookieStore
 }
-
-type (
-	SessionInfo struct {
-		Id   uint64
-		Name string
-	}
-)
 
 func Init() (err error) {
 	Core.DB, err = sql.Open("postgres", config.Conf.Postgresql)
@@ -38,43 +26,4 @@ func Init() (err error) {
 	Core.Store = sessions.NewCookieStore([]byte(config.Conf.SessionKey))
 
 	return nil
-}
-
-var mc *memcache.Client
-
-func InitSession() {
-	rand.Seed(time.Now().UnixNano())
-	mc = memcache.New(config.Conf.Memcache)
-}
-
-func CreateSession(info *SessionInfo) (id string, err error) {
-	var datas []byte
-	datas, err = json.Marshal(info)
-	if err != nil {
-		return
-	}
-
-	id = fmt.Sprint(rand.Int63())
-	err = mc.Set(&memcache.Item{Key: "session_" + id, Value: datas})
-	if err != nil {
-		id = ""
-		return
-	}
-
-	return
-}
-
-func GetSessionInfo(id string) (result *SessionInfo, err error) {
-	var item *memcache.Item
-	item, err = mc.Get("session_" + id)
-	if err != nil {
-		return
-	}
-
-	datas := item.Value
-
-	result = new(SessionInfo)
-	err = json.Unmarshal(datas, &result)
-
-	return
 }
