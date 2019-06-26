714406fdf5282f0a21228767032f851f6e3d37c7
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-06-26 14:23:16 +0400

    Added User, Task and ViewData structs

---
+++ data/data.go
@@ -0,0 +1,29 @@
+package data
+
+//User struct describe user
+type User struct {
+	UserID         int
+	UserName       string
+	UserFIO        string
+	UserDepartment int
+	UserGroup      int
+	UserisAdmin    bool
+	UserRate       int
+}
+
+//Task struct describe tasks
+type Task struct {
+	TaskID        int
+	TaskName      string
+	TaskDesc      string
+	TaskStat      int
+	TaskDateStart string
+	TaskDateEnd   string
+	TaskRate      int
+}
+
+//ViewData struct to show data on page
+type ViewData struct {
+	user User
+	task Task
+}
