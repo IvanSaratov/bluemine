9e1b56af35e874af34a2e94656c9c4495f3249a9
ivan <dfeqcn11@gmail.com>
2019-06-30 22:44:01 +0400

    Some rough method(not working)

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/IvanSaratov/bluemine/data"
+	"github.com/IvanSaratov/bluemine/server"
 )
 
 //UserProfileHandler handle user's profile page
@@ -29,8 +30,37 @@ func TasksHandler(w http.ResponseWriter, r *http.Request) {
 			UserFIO:        "test_testovich",
 			UserDepartment: "Otdel_Debilov",
 		},
+		TaskData: data.Task{
+			TaskName: "test",
+			TaskExecuter: 1337,
+			TaskStat: 1,
+		},
 	}
 
 	tmpl, _ := template.ParseFiles("public/html/tasks.html")
 	tmpl.Execute(w, data)
+}
+
+func readTasks() error {
+	rows, err := server.Core.DB.Query("SELECT task_name, stat, executor_id FROM tasks")
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	var tasksList []data.Task
+	for rows.Next() {
+		var name string
+		var stat int
+		var executerID int
+
+		err = rows.Scan(&name, &stat, &executerID)
+		if err != nil {
+			return err
+		}
+
+		tasksList = append(tasksList, data.Task{TaskName: name, TaskStat: stat, TaskExecuter: executerID})
+	}
+
+	return nil
 }
