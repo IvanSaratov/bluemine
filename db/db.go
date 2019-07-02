e3aa2d894c440050461f73bd9572c60a13bef4cb
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-07-02 22:33:15 +0400

    Added getting all Users and All Groups functions

--- db/db.go
+++ db/db.go
@@ -30,3 +30,57 @@ func GetUserInfo(DB *sql.DB, login string) (data.User, error) {
 
 	return user, nil
 }
+
+//GetAllUsers gets all users from DB
+func GetAllUsers(DB *sql.DB) ([]data.User, error) {
+	var users []data.User
+
+	stmt := "SELECT * FROM profiles"
+	rows, err := DB.Query(stmt)
+	if err != nil {
+		return users, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user data.User
+		err = rows.Scan(&user.UserID, &user.UserName, &user.UserFIO, &user.UserisAdmin, &user.UserRate)
+		if err != nil {
+			return users, err
+		}
+
+		users = append(users, user)
+	}
+	if rows.Err() != nil {
+		return users, err
+	}
+
+	return users, nil
+}
+
+//GetAllGroups gets all users from DB
+func GetAllGroups(DB *sql.DB) ([]data.Group, error) {
+	var groups []data.Group
+
+	stmt := "SELECT * FROM groups"
+	rows, err := DB.Query(stmt)
+	if err != nil {
+		return groups, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var group data.Group
+		err = rows.Scan(&group.GroupID, &group.GroupName)
+		if err != nil {
+			return groups, err
+		}
+
+		groups = append(groups, group)
+	}
+	if rows.Err() != nil {
+		return groups, err
+	}
+
+	return groups, nil
+}
