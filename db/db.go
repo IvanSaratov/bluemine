4cd1f4522974fab5afc4abff0c2072b57cbc6e3d
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-07-02 10:53:55 +0400

    Added Register function

--- db/db.go
+++ db/db.go
@@ -4,6 +4,18 @@ import (
 	"database/sql"
 )
 
+//RegisterUser adds user to DB
+func RegisterUser(DB *sql.DB, login, userFIO string) error {
+	stmt := "INSERT INTO profiles (id, username, user_fio) VALUES (DEFAULT, $1, $2) RETURNING id"
+	var userID int64
+	err := DB.QueryRow(stmt, login, userFIO).Scan(&userID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func prepareStmt(db *sql.DB, stmt string) (*sql.Stmt, error) {
 	res, err := db.Prepare(stmt)
 	if err != nil {
