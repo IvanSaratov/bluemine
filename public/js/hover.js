fb6927259b63f5ba2485a5c6df12c2ff02f5ab2f
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-07-06 19:33:50 +0400

    Changed hovering function

--- public/js/hover.js
+++ public/js/hover.js
@@ -1,14 +1,16 @@
-$('#ico_new').hover(
-    function(){
-        $('#new_item').css('display', 'block');
-    },
-    function(){
-        $('#new_item').css('display', 'none');
-    }
-)
-
-$('#new_item').hover(
-    function(){
-        $('#iconew').toggleClass("hovered", 200);
-    }
-);
+$(document).ready(function() {
+    $('#ico_new').hover(
+        function() {
+            $('#new_item').css('display', 'block');
+        },
+        function() {
+            $('#new_item').css('display', 'none');
+        }
+    )
+    
+    $('#new_item').hover(
+        function() {
+            $('#iconew').toggleClass("hovered", 200);
+        }
+    );
+});
