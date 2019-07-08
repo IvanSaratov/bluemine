d6ac7afc314efaa4df453cdb87904b9dff3bd326
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-07-09 00:31:48 +0400

    Added templates for add_new_tmpl and add_new_group

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,27 @@
 $('#new_task').hide(0);
+$('#new_tmpl').hide(0);
+$('#new_group').hide(0);
 
 $('#add_new_task').click(function() {
     $('#new_task').show(300);
 });
 
-$('#new_task_close').click(function() {
+$('#add_new_tmpl').click(function() {
+    $('#new_tmpl').show(300);
+});
+
+$('#add_new_group').click(function() {
+    $('#new_group').show(300);
+});
+
+$('#new_task .new_item_close').click(function() {
     $('#new_task').hide(300);
+});
+
+$('#new_tmpl .new_item_close').click(function() {
+    $('#new_tmpl').hide(300);
+});
+
+$('#new_group .new_item_close').click(function() {
+    $('#new_group').hide(300);
 });
