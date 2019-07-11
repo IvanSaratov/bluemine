0bb39347c11720450d0f17597a46d992ecb1107f
ivan <dfeqcn11@gmail.com>
2019-07-11 15:56:16 +0400

    Rework temlate parse function

--- server/server.go
+++ server/server.go
@@ -16,6 +16,16 @@ var Core struct {
 	Templates map[string]*template.Template
 }
 
+var (
+	layoutHTML = "public/html/layout.html"
+	newItemHTML = "public/html/newItem.html"
+	tasksHTML = "public/html/tasks.html"
+	profileHTML = "public/html/profile.html"
+	taskPageHTML = "public/html/taskpage.html"
+	groupHTML = "public/html/group.html"
+	groupsHTML = "public/html/groups.html"
+)
+
 //Init function initializes server
 func Init() {
 	Core.DB = sqlx.MustConnect("postgres", "user="+config.Conf.DBUser+" password="+config.Conf.DBPassword+" host="+config.Conf.Host+" port="+config.Conf.DBPort+" dbname="+config.Conf.DBName)
@@ -33,15 +43,15 @@ func Init() {
 	log.Println("Created cookie store")
 
 	Core.Templates = make(map[string]*template.Template)
-	temp := template.Must(template.ParseFiles("public/html/layout.html", "public/html/tasks.html"))
+	temp := template.Must(template.ParseFiles(layoutHTML, newItemHTML, tasksHTML))
 	Core.Templates["tasks"] = temp
-	temp = template.Must(template.ParseFiles("public/html/layout.html", "public/html/profile.html"))
+	temp = template.Must(template.ParseFiles(layoutHTML, newItemHTML, profileHTML))
 	Core.Templates["profile"] = temp
-	temp = template.Must(template.ParseFiles("public/html/layout.html", "public/html/taskpage.html"))
+	temp = template.Must(template.ParseFiles(layoutHTML, newItemHTML, taskPageHTML))
 	Core.Templates["taskPage"] = temp
-	temp = template.Must(template.ParseFiles("public/html/layout.html", "public/html/group.html"))
+	temp = template.Must(template.ParseFiles(layoutHTML, newItemHTML, groupHTML))
 	Core.Templates["group"] = temp
-	temp = template.Must(template.ParseFiles("public/html/layout.html", "public/html/groups.html"))
+	temp = template.Must(template.ParseFiles(layoutHTML, newItemHTML, groupsHTML))
 	Core.Templates["groups"] = temp
 
 	log.Println("All templates parsed")
