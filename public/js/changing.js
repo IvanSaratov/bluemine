e5d335072497e201cc06a9f455eac65c70aef030
ivan <dfeqcn11@gmail.com>
2019-07-12 13:57:32 +0400

    Merged js scripts changing adn changeGroup, added button

--- public/js/changing.js
+++ public/js/changing.js
@@ -22,3 +22,18 @@ function tChange() {
         })
     }
 }
+
+function gChange() {
+    var href = location.href.split('/');
+    var id = href[5];
+    $.get("/groupchange", { id: id }).done(function(data){
+        $('#task_change').show(0);
+        $('#task_send').hide(0);
+        $('#new_group').show(300);
+        $('#input_group_name').val(data.GroupName)
+
+        for (i = 0; i < data.GroupMembers.length; i++) {
+            $("input[value='"+data.GroupMembers[i].UserName+"']").prop('checked', true);
+        };
+    })
+};
