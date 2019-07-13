eb57e094a03ec5dc79fb28ae1b3b9e2da8db3ff9
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-07-13 23:25:46 +0400

    Fixed line-ending in html

--- public/js/gettaskdesc.js
+++ public/js/gettaskdesc.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var href = location.href.split('/');
     var id = href[5];
     $.get("/gettaskdesc", { id: id }).done(function(data){
-        descWrap.text(data);
+        data = data.replace(/(?:\r\n|\r|\n)/g, '<br>');
+        descWrap.html(data);
     })
 });
