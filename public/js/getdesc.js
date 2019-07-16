0d94675610915aedd1ae475ed26ea1229f663f31
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-07-16 13:50:17 +0400

    Changed view of desciption on task page

--- public/js/getdesc.js
+++ public/js/getdesc.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
+    var md = window.markdownit();
+
     var descWrap = $('#task_desc .value');
     var href = location.href.split('/');
     var id = href[5];
     var type = href[3];
 
     $.get("/getdesc", { id: id, type:type }).done(function(data){
-        data = data.replace(/(?:\r\n|\r|\n)/g, '<br>');
-        descWrap.html(data);
+        var html = md.render(data);
+        descWrap.html(html);
     })
 });
