37cab372847c2820177b1ccfe0af2f3c3692bb76
ivan <dfeqcn11@gmail.com>
2019-07-16 12:03:25 +0400

    Rename getDesc script, added some field in wiki

--- public/js/getdesc.js
+++ public/js/getdesc.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
     var descWrap = $('#task_desc .value');
     var href = location.href.split('/');
     var id = href[5];
-    $.get("/gettaskdesc", { id: id }).done(function(data){
+    var type = href[3];
+    console.log(type)
+    $.get("/getdesc", { id: id, type:type }).done(function(data){
         data = data.replace(/(?:\r\n|\r|\n)/g, '<br>');
         descWrap.html(data);
     })
