b66a8477463d7c636f4b8b6aa65e02db0180a43b
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-07-17 23:24:40 +0400

    Added sorting script for name and status

---
+++ public/js/sorting.js
@@ -0,0 +1,72 @@
+function customStat(status) {
+    switch (status) {
+        case "Новая": {
+            return 0;
+            break;
+        }
+        case "В процессе": {
+            return 1;
+            break;
+        }
+        case "Отложена": {
+            return 2;
+            break;
+        }
+        case "Закрыта": {
+            return 3;
+            break;
+        }
+    }
+}
+
+$(document).ready(function() {
+    $('#sort_stat_down').click()
+})
+
+var tableID;
+
+$('#sort_name_down').click(function() {
+    tableID = $(this).closest('table').attr('id');
+    $('#' + tableID + ' .sort_icon').css('color', 'white')
+    $(this).css('color', 'rgb(54, 161, 248)')
+
+    $('#' + tableID + ' tbody tr').sort(function (a, b) {
+        return $(a).find('#name a').text() < $(b).find('#name a').text()
+    }).appendTo('#' + tableID + ' tbody')
+})
+
+$('#sort_name_up').click(function() {
+    tableID = $(this).closest('table').attr('id');
+    $('#' + tableID + ' .sort_icon').css('color', 'white')
+    $(this).css('color', 'rgb(54, 161, 248)')
+
+    $('#' + tableID + ' tbody tr').sort(function (a, b) {
+        return $(a).find('#name a').text() > $(b).find('#name a').text()
+    }).appendTo('#' + tableID + ' tbody')
+})
+
+$('#sort_stat_down').click(function() {
+    tableID = $(this).closest('table').attr('id');
+    $('#' + tableID + ' .sort_icon').css('color', 'white')
+    $(this).css('color', 'rgb(54, 161, 248)')
+
+    $('#' + tableID + ' tbody tr').sort(function (a, b) {
+        aVal = customStat($(a).find('#stat').text())
+        bVal = customStat($(b).find('#stat').text())
+
+        return aVal > bVal
+    }).appendTo('#' + tableID + ' tbody')
+})
+
+$('#sort_stat_up').click(function() {
+    tableID = $(this).closest('table').attr('id');
+    $('#' + tableID + ' .sort_icon').css('color', 'white')
+    $(this).css('color', 'rgb(54, 161, 248)')
+
+    $('#' + tableID + ' tbody tr').sort(function (a, b) {
+        aVal = customStat($(a).find('#stat').text())
+        bVal = customStat($(b).find('#stat').text())
+
+        return aVal < bVal
+    }).appendTo('#' + tableID + ' tbody')
+})
