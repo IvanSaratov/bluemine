6f57f987fb58be45ef662eb0298ef5d156a4434b
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-07-17 16:49:44 +0400

    Auto padding for child elements in tree

--- public/js/getwikilist.js
+++ public/js/getwikilist.js
@@ -6,6 +6,7 @@ $.get("/getwikilist", function(data) {
                 var wrap = $('#wikilist #main');
                 html = '<div id="' + data[i].WikiIDStr + '"><div class="item"><a href="/wiki/show/' + data[i].WikiIDStr + '">' + data[i].WikiName + '</a></div></div>'
                 $(wrap).append(html);
+                $('#' + data[i].WikiIDStr).children('.item').css("padding-left", '15px')
             }
         } else {
             if (!nodeExists(data[i].WikiIDStr)) {
@@ -14,6 +15,9 @@ $.get("/getwikilist", function(data) {
                 var wrap = $('#wikilist #main #' + data[i].WikiFatherIDStr + ' #child_of_' + data[i].WikiFatherIDStr);
                 html = '<div id="' + data[i].WikiIDStr + '"><div class="nested_item"><a href="/wiki/show/' + data[i].WikiIDStr + '">' + data[i].WikiName + '</a></div></div>'
                 $(wrap).append(html);
+                var par = $('#' + data[i].WikiIDStr).parents().length
+                var padding = 15 + 10*((par - 8)/2 + 1);
+                $('#' + data[i].WikiIDStr).children('.nested_item').css("padding-left", padding.toString() + 'px')
             }
         }
     }
