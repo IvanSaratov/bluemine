1aa6321fe5ad2d73244d193204f9f7c2484fe43b
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-07-25 11:45:44 +0400

    Deleted old script file

--- public/js/autoparse.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('#input_desc').bind('input propertychange', function() {
-        var markdown = $('#input_desc').val();
-        MDParse(markdown, $('#markdown_output'))
-    })
-})
