ebe92a01d68a0fa7c5a3fd694f375171969a6aca
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-07-29 13:33:40 +0400

    Fixed hiding checklist

--- public/js/main.js
+++ public/js/main.js
@@ -43,9 +43,9 @@ function MDParse(text, wrap) {
 }
 
 function addCheckbox() {
-    var checkName = $('#input_task_checkbox').val();
+    var checkName = $('#input_checkbox').val();
     var html = '<div style="width=100%;"><input class="checkbox" type="checkbox" name="checkbox" value="' + checkName + '"><label for="' + checkName + '">' + checkName + '</label></div>';
-    $('#task_checklist_wrap').append(html);
+    $('#checklist_wrap').append(html);
 }
 
 function getTaskDescOrWikiArticle(type, id) {
